8099ca898c74d0bac08d45188d0ef25c1467a3a8
Aniket Naik <eraniket26@gmail.com>
2026-08-02 16:46:50 +0530

    Dummy commit

---
+++ src/main/java/org/spring/maven/App.java
@@ -0,0 +1,13 @@
+package org.spring.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
